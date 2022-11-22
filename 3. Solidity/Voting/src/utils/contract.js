adc6d0bf9bb95e29845532196e9635d3d5d271b7
Goulven <goulven.friess@gmail.com>
2022-11-21 20:52:02 -0400

    modif contract

--- 3. Solidity/Voting/src/utils/contract.js
+++ 3. Solidity/Voting/src/utils/contract.js
@@ -1,6 +1,6 @@
 
 // The smart contract address :
-export const votingAddress = "0x001";
+export const votingAddress = "0xA92eA5b6C612a2C05A9fD75fe18F75a14445F535";
 
 // The owner's address :
-export const adminAddress="0x002";
+export const adminAddress="0x62490c9e6e16e78cc9b92262a671996b51fb2418";
